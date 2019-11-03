21a0185f6bacc013edf04391b98b01ac728baa51
Adam <PseudoDesign@users.noreply.github.com>
2019-11-03 02:08:01 -0700

    removed SetUp

--- tests/test_github_pr_dump.py
+++ tests/test_github_pr_dump.py
@@ -4,5 +4,4 @@ from unittest.mock import patch
 
 @patch('github_log_dump.api_access.github_login')
 class TestGithubAPIAccess(TestCase):
-    def setUp(self, mock_github_login) -> None:
-        self.github_instance = mock_github_login()
+    pass
